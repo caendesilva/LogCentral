605e3b7bf4b84ddf6febacf544bef25382fbd5ff
Caen De Silva <caen@desilva.se>
2022-03-12 22:08:11 +0100

    Add the Sanctum auth middleware

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/logs', LogController::class);
+Route::apiResource('/logs', LogController::class)->middleware('auth:sanctum');
